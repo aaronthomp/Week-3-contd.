04e804b6898bf96e6190063f0c8d8c15dbdb040c
Aaron Thompson <thom1583@algonquinlive.com>
2024-02-18 20:34:22 -0500

    Added JavaDoc comments and finished 5 test cases

--- app/src/main/java/com/example/testdesigntab/MainActivity.java
+++ app/src/main/java/com/example/testdesigntab/MainActivity.java
@@ -3,12 +3,105 @@ package com.example.testdesigntab;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+/**
+ * this class runs the code since its the main
+ * @author Aaron Thompson
+ * @version 1.0
+ */
 public class MainActivity extends AppCompatActivity {
+    /**
+     * this holds the text at the center of the screen
+     */
+    private TextView tv = null;
+    /**
+     * this holds the edit text for the password
+     */
+    private EditText et = null;
+    /**
+     * this holds the button
+     */
+    private Button btn = null;
+    /**
+     * This method checks to see if the password input has enough complexity
+     * @param pw takes in a password
+     * @return returns true if it meets the complexity requirements
+     */
+    boolean checkPasswordComplexity(String pw) {
+        boolean foundUpperCase, foundLowerCase, foundNumber, foundSpecial;
+
+        foundUpperCase = foundLowerCase = foundNumber = foundSpecial = false;
+
+        for (char c : pw.toCharArray()) {
+            if (Character.isUpperCase(c)) {
+                foundUpperCase = true;
+            } else if (Character.isLowerCase(c)) {
+                foundLowerCase = true;
+            } else if (Character.isDigit(c)) {
+                foundNumber = true;
+            } else if (isSpecialCharacter(c)) {
+                foundSpecial = true;
+            }
+        }
+
+        if (!foundUpperCase) {
+            Toast.makeText(this, "Your password does not have an upper case letter", Toast.LENGTH_SHORT).show();
+            return false;
+        } else if (!foundLowerCase) {
+            Toast.makeText(this, "Your password does not have a lower case letter", Toast.LENGTH_SHORT).show();
+            return false;
+        } else if (!foundNumber) {
+            Toast.makeText(this, "Your password does not have a number", Toast.LENGTH_SHORT).show();
+            return false;
+        } else if (!foundSpecial) {
+            Toast.makeText(this, "Your password does not have a special symbol", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        return true;
+    }
+
+    // Function to check if the character is a special character
+    private boolean isSpecialCharacter(char c) {
+        switch (c) {
+            case '#':
+            case '$':
+            case '%':
+            case '^':
+            case '&':
+            case '*':
+            case '!':
+            case '@':
+            case '?':
+                return true;
+            default:
+                return false;
+        }
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView tv = findViewById(R.id.textView);
+        EditText et = findViewById(R.id.editTextTextPassword);
+        Button btn = findViewById(R.id.buttonLogin);
+
+        btn.setOnClickListener((clk ->{
+            String password = et.getText().toString();
+
+            boolean isPasswordValid = checkPasswordComplexity(password);
+            if (isPasswordValid) {
+                tv.setText("Your password meets the requirements");
+            } else {
+                tv.setText("You shall not pass!");
+            }
+        }));
     }
 }
