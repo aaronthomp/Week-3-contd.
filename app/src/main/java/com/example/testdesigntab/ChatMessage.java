3e6f9451d3c139ba39e273477b56b96fcff4f610
Aaron Thompson <thom1583@algonquinlive.com>
2024-03-13 16:41:50 -0400

    Project works as intended however receive does not fully align on right** side of phone, may fix this if necessary

---
+++ app/src/main/java/com/example/testdesigntab/ChatMessage.java
@@ -0,0 +1,21 @@
+package com.example.testdesigntab;
+
+public class ChatMessage {
+    String message;
+    String timeSent;
+    boolean isSentButton;
+    public ChatMessage(String message, String timeSent, boolean isSentButton) {
+        this.message = message;
+        this.timeSent = timeSent;
+        this.isSentButton = isSentButton;
+    }
+    public String getMessage() {
+        return message;
+    }
+    public String getTimeSent() {
+        return timeSent;
+    }
+    public boolean isSentButton() {
+        return isSentButton;
+    }
+}
